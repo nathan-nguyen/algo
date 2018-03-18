961526c54210339868af8a2be32de3335b98be14
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-03-19 02:33:46 +0800

    Added prefix-neighbors folder

---
+++ prefix-neighbors_/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args){
+		Solution solution = new Solution();
+		solution.solve();
+	}
+
+	private void solve(){
+		Scanner in = new Scanner(System.in);
+	}
+}
