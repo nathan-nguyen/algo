93c00aa112b520dc0346e26a45dc37bc2e41711e
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-04-14 18:29:43 +0800

    Fixed bug when cat 2 files

--- download.sh
+++ download.sh
@@ -2,11 +2,13 @@
 
 User=$1
 
-curl https://leetcode.com/${User}/ >> tmp
-cat ${User}.userlist >> tmp
+curl https://leetcode.com/${User}/ > tmp1
 
-grep problems tmp | tr -d " " | cut -c10- | tr -d "\"" | sort | uniq > ${User}.userlist
+grep problems tmp1 | tr -d " " | cut -c10- | tr -d "\"" > tmp2
+cat ${User}.userlist tmp2 | sort | uniq > tmp1
+
+mv tmp1 ${User}.userlist
 
 # Remove tmp file
-rm -f tmp
+rm -f tmp1 tmp2
 
