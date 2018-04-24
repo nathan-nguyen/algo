2a7eb908a669654070e4e12ed0aa352dceecefca
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-04-24 21:38:27 +0800

    Added shorter solution for First Missing Positive

--- [Hard] first-missing-positive_/README.md
+++ [Hard] first-missing-positive_/README.md
@@ -2,3 +2,5 @@
 
 This problem is similar to problem find-the-smallest-positive-number-missing-from-an-unsorted-array in geeksforgeeks
 
+However, I followed the nicer solution in leetcode.
+
--- [Hard] first-missing-positive_/Solution.java
+++ [Hard] first-missing-positive_/Solution.java
@@ -1,43 +1,18 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        if (nums.length == 0) return 1;
-
-	// Rearange the array
-	int last = nums.length - 1;
-	int start = 0;
-	for (;start <= last; ++start){
-		if (nums[start] <= 0) continue;
-                
-		// nums[start] > 0
-		while (last >= 0 && nums[last] > 0) --last;
-
-		// All element > 0
-		if (last < 0) break;
-
-		// nums[last] <= 0 - last always != start
-		if (last < start) break;
-
-		int tmp = nums[last];
-		nums[last] = nums[start];
-		nums[start] = tmp;
-	}
-
-        if (start == nums.length) return 1;
-            
-	for (int i = 0; i < start; ++i) nums[i] = 1;
-
-	for (int i = start; i < nums.length; ++i){
-		// nums[i] > 0
-		if (Math.abs(nums[i]) - 1 < nums.length) {
-			nums[Math.abs(nums[i]) - 1] = - Math.abs(nums[Math.abs(nums[i]) - 1]);
+	public int firstMissingPositive(int[] nums) {
+		for (int i = 0; i < nums.length; ++i){
+			while (nums[i] > 0 && nums[i] - 1 < nums.length && nums[i] != nums[nums[i] - 1]){
+				int index = nums[i] - 1;
+				nums[i] ^= nums[index];
+				nums[index] ^= nums[i];
+				nums[i] ^= nums[index];
+			}
 		}
-	}
 
-	for (int i = 0; i < nums.length; ++i){
-		if (nums[i] > 0) return i + 1;
+		for (int i = 0; i < nums.length; ++i){
+			if (nums[i] != i + 1) return i + 1;
+		}
+
+		return nums.length + 1;
 	}
-	
-        return nums.length + 1;
-    }
 }
-
