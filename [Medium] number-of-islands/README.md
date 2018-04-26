d0fba1fdeda1d9d48eb76d4e4889fd03f65da3c1
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-04-26 22:33:24 +0800

    Added correct solution for Number of Islands

---
+++ [Medium] number-of-islands/README.md
@@ -0,0 +1,4 @@
+# Number of Islands
+
+This problem, I used union find approach.
+
--- [Medium] number-of-islands/Solution.java
+++ [Medium] number-of-islands/Solution.java
@@ -5,15 +5,46 @@ class Solution {
 
 		int m = grid[0].length;
 
+		int[] u = new int[n * m];
+		for (int i = 0; i < u.length; ++i) u[i] = i;
+
+		int[] sz = new int[n * m];
+		Arrays.fill(sz, 1);
+
 		int count = 0;
 		for (int i = 0; i < n; ++i){
 			for (int j = 0; j < m; ++j){
 				if (grid[i][j] == '0') continue;
-				if (i >= 1 && grid[i-1][j] == '1') continue;
-				if (j >= 1 && grid[i][j-1] == '1') continue;
-				++count;
+				int k = i * n + j;
+				boolean found = false;
+				if (i >= 1 && grid[i-1][j] == '1') {
+					int k1 = (i - 1) * n + j;
+					union(u, sz, k1, k);
+					found = true;
+				}
+				if (j >= 1 && grid[i][j-1] == '1') {
+					int k1 = i * n + (j - 1);
+					if (!union(u, sz, k1, k) && found) --count;
+					found = true;
+				}
+				if (!found) ++count;
 			}
 		}
 		return count;
+	}
+
+	private boolean union(int[] u, int[] sz, int k1, int k2){
+		while (u[k1] != k1) k1 = u[k1];
+                while (u[k2] != k2) k2 = u[k2];
+                if (k1 == k2) return true;
+                else if (sz[k1] < sz[k2]) {
+                	u[k1] = k2;
+                        sz[k2] += sz[k1];
+                }
+                else {
+                        u[k2] = k1;
+                	sz[k1] += sz[k2];
+                }
+		return false;
 	}
 }
