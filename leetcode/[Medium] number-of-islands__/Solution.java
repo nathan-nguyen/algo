cbb8293172d182b3fa96add94b9c45b61d638571
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-05-20 10:53:00 +0800

    Added O(n) solution for Number of Islands

--- leetcode/[Medium] number-of-islands__/Solution.java
+++ leetcode/[Medium] number-of-islands__/Solution.java
@@ -1,50 +1,28 @@
 class Solution {
 	public int numIslands(char[][] grid) {
+		if (grid.length == 0 || grid[0].length == 0) return 0;
+
 		int n = grid.length;
-		if (n == 0) return 0;
-
 		int m = grid[0].length;
-
-		int[] u = new int[n * m];
-		for (int i = 0; i < u.length; ++i) u[i] = i;
-
-		int[] sz = new int[n * m];
-		Arrays.fill(sz, 1);
 
 		int count = 0;
 		for (int i = 0; i < n; ++i){
 			for (int j = 0; j < m; ++j){
 				if (grid[i][j] == '0') continue;
-				int k = i * n + j;
-				boolean found = false;
-				if (i >= 1 && grid[i-1][j] == '1') {
-					int k1 = (i - 1) * n + j;
-					union(u, sz, k1, k);
-					found = true;
-				}
-				if (j >= 1 && grid[i][j-1] == '1') {
-					int k1 = i * n + (j - 1);
-					if (!union(u, sz, k1, k) && found) --count;
-					found = true;
-				}
-				if (!found) ++count;
+				++count;
+				dfs(i, j, grid);
 			}
 		}
 		return count;
 	}
 
-	private boolean union(int[] u, int[] sz, int k1, int k2){
-		while (u[k1] != k1) k1 = u[k1];
-                while (u[k2] != k2) k2 = u[k2];
-                if (k1 == k2) return true;
-                else if (sz[k1] < sz[k2]) {
-                	u[k1] = k2;
-                        sz[k2] += sz[k1];
-                }
-                else {
-                        u[k2] = k1;
-                	sz[k1] += sz[k2];
-                }
-		return false;
+	private void dfs(int i, int j, char[][] grid){
+		if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+
+		grid[i][j] = '0';
+		dfs(i - 1, j, grid);
+		dfs(i, j - 1, grid);
+		dfs(i, j + 1, grid);
+		dfs(i + 1, j, grid);
 	}
 }
