92f10e54a6c3351ed915d66c41e84970defa161b
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-02 08:14:18 +0800

    Added better solution for Longest Univalue Path

--- leetcode/[Easy] longest-univalue-path/Solution.java
+++ leetcode/[Easy] longest-univalue-path/Solution.java
@@ -8,34 +8,30 @@
  * }
  */
 class Solution {
+	private int max;
 	public int longestUnivaluePath(TreeNode root) {
 		if (root == null) return 0;
-		return lUP(root)[0] - 1;
+
+		max = 0;
+		lUP(root);
+
+		return max;
 	}
 
 	// 0 - normal Max
 	// 1 - max with root value
-	public int[] lUP(TreeNode root){
-		if (root == null) return new int[]{0, 0};
+	public int lUP(TreeNode root){
+		if (root == null) return 0;
 
-                int[] left = lUP(root.left);
-                int[] right = lUP(root.right);
+                int left = lUP(root.left);
+                int right = lUP(root.right);
 
-		int[] max = new int[2];
-		max[0] = Math.max(left[0], right[0]);
+		if (root.left != null && root.left.val == root.val) ++left;
+		else left = 0;
+		if (root.right != null && root.right.val == root.val) ++right;
+		else right = 0;
 
-		int maxRootPath = 1;
-		max[1] = 1;
-		if (root.left != null && root.left.val == root.val) {
-			maxRootPath += left[1];
-			max[1] = Math.max(max[1], 1 + left[1]);
-		}
-		if (root.right != null && root.right.val == root.val) {
-			maxRootPath += right[1];
-			max[1] = Math.max(max[1], 1 + right[1]);
-		}
-
-		max[0] = Math.max(max[0], maxRootPath);
-		return max;
+		max = Math.max(max, left + right);
+		return Math.max(left, right);
 	}
 }
