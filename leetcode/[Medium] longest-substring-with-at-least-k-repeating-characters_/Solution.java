7f1079c07ebd368e6c9d8ff9aa91b8f6aba0f534
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-05 08:18:00 +0800

    Added Longest Substring with At Least K Repeating Characters

--- leetcode/[Medium] longest-substring-with-at-least-k-repeating-characters_/Solution.java
+++ leetcode/[Medium] longest-substring-with-at-least-k-repeating-characters_/Solution.java
@@ -1,5 +1,31 @@
 class Solution {
 	public int longestSubstring(String s, int k) {
-		
+		if (s.length() < k) return 0;
+
+		int[] c = new int[26];
+		for (int i = 0; i < s.length(); ++i) ++c[s.charAt(i) - 'a'];
+
+		boolean found = false;
+		for (int i = 0; i < 26; ++i) {
+			if (c[i] > 0 && c[i] < k) {
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) return s.length();
+
+		int max = 0;
+		int begin = 0;
+
+		for (int i = 0; i < s.length(); ++i){
+			int index = s.charAt(i) - 'a';
+			if (c[index] > 0 && c[index] < k){
+				max = Math.max(max, longestSubstring(s.substring(begin, i), k));
+				++begin;
+			}
+		}
+		max = Math.max(max, longestSubstring(s.substring(begin, s.length()), k));
+		return max;
 	}
 }
