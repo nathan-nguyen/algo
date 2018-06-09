e9ed761aab85b5a240ab2c6f1456870e02130c7c
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-10 00:05:13 +0800

    Added shorter but harder to maintain solution for Sudoku Solver

--- leetcode/[Hard] sudoku-solver__/Solutions/Solution.java
+++ leetcode/[Hard] sudoku-solver__/Solutions/Solution.java
@@ -3,41 +3,41 @@ import java.util.HashMap;
 
 class Solution {
 	public void solveSudoku(char[][] board) {
-		HashMap<String, HashSet<Integer>> map = new HashMap<>();
-
-		for (int i = 0; i < 9; ++i){
-			map.put("r" + i, createDefaultHashSet());
-			map.put("c" + i, createDefaultHashSet());
-			for (int j = 0; j < 9; ++j) {
-				if (i < 3 && j < 3) map.put("gr" + i + "_" + j, createDefaultHashSet());
-				map.put(i + "_" + j, createDefaultHashSet());
-			}
-		}
-
+		boolean[][] row = new boolean[9][10];
+		boolean[][] col = new boolean[9][10];
+		boolean[][][] gr = new boolean[3][3][10];
+		
 		for (int i = 0; i < 9; ++i){
 			for (int j = 0; j < 9; ++j){
-				if (board[i][j] != '.') remove(i, j, map, board[i][j] - '0');
+				if (board[i][j] != '.') {
+                                        int val = board[i][j] - '0';
+                                        row[i][val] = col[j][val] = gr[i/3][j/3][val] = true;
+                                }
 			}
 		}
 	
-		solve(0, board, map);	
+		solve(0, board, row, col, gr);	
 	}
 
-	private boolean solve(int start, char[][] board, HashMap<String, HashSet<Integer>> map){
+	private boolean solve(int start, char[][] board, boolean[][] row, boolean[][] col, boolean[][][] gr){
+		if (start == 81) return true;
+
 		for (int i = start; i < 81; ++i){
 			int x = i / 9;
 			int y = i % 9;
 			if (board[x][y] != '.') continue;
-			if (!isValid(x, y, map)) continue;
 
 			for (int k = 1; k <= 9; ++k){
-				if (!contains(x, y, map, k)) continue;
+				if (row[x][k] || col[y][k] || gr[x/3][y/3][k]) continue;
+
 				board[x][y] = (char)('0' + k);
-				if (i == 80) return true;
 				
-				remove(x, y, map, k);
-				if (solve(i + 1, board, map)) return true;
-				add(x, y, map, k);
+				row[x][k] = col[y][k] = gr[x/3][y/3][k] = true;
+                                
+				if (solve(i + 1, board, row, col, gr)) return true;
+                                
+				row[x][k] = col[y][k] = gr[x/3][y/3][k] = false;
+                                
 				board[x][y] = '.';
 			}
 			return false;
@@ -45,31 +45,5 @@ class Solution {
 		return true;
 	}
 
-	private void add(int x, int y, HashMap<String, HashSet<Integer>> map, int val){
-		map.get("r" + x).add(val);
-		map.get("c" + y).add(val);
-		map.get("gr" + (x / 3) + "_" + (y / 3)).add(val);
-		map.get(x + "_" + y).add(val);
-	}
-
-	private void remove(int x, int y, HashMap<String, HashSet<Integer>> map, int val){
-                map.get("r" + x).remove(val);
-                map.get("c" + y).remove(val);
-                map.get("gr" + (x / 3) + "_" + (y / 3)).remove(val); 
-                map.get(x + "_" + y).remove(val);
-        }
-
-	private boolean isValid(int x, int y, HashMap<String, HashSet<Integer>> map){
-		return map.get("r" + x).size() > 0 && map.get("c" + y).size() > 0 && map.get("gr" + (x / 3) + "_" + (y / 3)).size() > 0 && map.get(x + "_" + y).size() > 0;
-	}	
-
-	private boolean contains(int x, int y, HashMap<String, HashSet<Integer>> map, int val){
-		return map.get("r" + x).contains(val) && map.get("c" + y).contains(val) && map.get("gr" + (x / 3) + "_" + (y / 3)).contains(val) && map.get(x + "_" + y).contains(val);
-	}
-
-	private HashSet<Integer> createDefaultHashSet(){
-		HashSet<Integer> set = new HashSet<>();
-                for (int k = 1; k <= 9; ++k) set.add(k);
-		return set;
-	}
 }
+
