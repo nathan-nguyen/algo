3e032de8fa323723b14906b9ebf7c37bfbdbadfb
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-13 05:50:38 +0800

    Added Word Break

--- leetcode/[Medium] word-break_/Solution.java
+++ leetcode/[Medium] word-break_/Solution.java
@@ -1,5 +1,17 @@
 class Solution {
 	public boolean wordBreak(String s, List<String> wordDict) {
-		
+		HashSet<String> set = new HashSet<>(wordDict);
+
+		boolean[] dp = new boolean[s.length() + 1];
+		dp[0] = true;
+
+		for (int i = 0; i <= s.length(); ++i){
+			if (!dp[i]) continue;
+			for (int j = i + 1; j <= s.length(); ++j){
+				if (dp[j]) continue;
+				dp[j] = set.contains(s.substring(i, j));
+			}
+		}
+		return dp[s.length()];
 	}
 }
