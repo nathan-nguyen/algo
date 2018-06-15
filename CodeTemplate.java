3d3f375869a2b66915a7a632087d9fca38a7c03a
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-15 13:33:29 +0800

    Added Segment Tree Template

--- CodeTemplate.java
+++ CodeTemplate.java
@@ -94,3 +94,79 @@ n ^ (1 << k)
 
 --------------------
 
+class SegmentTree {
+	int st[]; //Array to store segment tree
+	int OUT_RANGE_VALUE = Integer.MAX_VALUE;
+ 
+	int function(int x, int y) {
+		return (x < y) ? x : y;
+	}
+ 
+	int getMid(int s, int e) {
+		return s + (e - s) / 2;
+	}
+
+
+	// ss - se - index 
+	int queryHelper(int ss, int se, int qs, int qe, int index) {
+		if (qs <= ss && qe >= se) return st[index];
+ 
+		if (se < qs || ss > qe) return OUT_RANGE_VALUE;
+ 
+		int mid = getMid(ss, se);
+		return function(queryHelper(ss, mid, qs, qe, 2 * index + 1), queryHelper(mid + 1, se, qs, qe, 2 * index + 2));
+	}
+ 
+	int query(int n, int qs, int qe) {
+		if (qs < 0 || qe > n - 1 || qs > qe) {
+			System.out.println("Invalid Input");
+			return -1;
+		}
+ 
+		return queryHelper(0, n - 1, qs, qe, 0);
+	}
+
+	int updateHelper(int ss, int se, int si, int i, int newVal) {
+		if (i < ss || i > se) return st[si];
+ 
+		if (se != ss) {
+			int mid = getMid(ss, se);
+			st[si] = function(updateHelper(ss, mid, 2 * si + 1, i, newVal), updateHelper(mid + 1, se, 2 * si + 2, i, newVal));
+		}
+		else st[si] = newVal;
+		return st[si];
+	}
+ 
+	void update(int arr[], int n, int i, int newVal){
+		if (i < 0 || i > n - 1) {
+			System.out.println("Invalid Input");
+			return;
+		}
+ 
+ 		arr[i] = newVal;
+		updateHelper(0, n - 1, 0, i, newVal);
+	}
+ 
+	int constructHelper(int arr[], int ss, int se, int si){
+		if (ss == se) {
+			st[si] = arr[ss];
+			return arr[ss];
+		}
+ 
+		int mid = getMid(ss, se);
+		st[si] = function(constructHelper(arr, ss, mid, si * 2 + 1), constructHelper(arr, mid + 1, se, si * 2 + 2));
+		return st[si];
+	}
+ 
+	SegmentTree(int arr[], int n) {
+		int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+ 
+		int max_size = 2 * (int) Math.pow(2, x) - 1;
+		st = new int[max_size]; // Allocate memory
+ 
+		constructHelper(arr, 0, n - 1, 0);
+	}
+}
+
+--------------------
+
