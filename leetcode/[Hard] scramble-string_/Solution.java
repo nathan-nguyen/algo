e3a7d28d5c037142e0702587e1b7ea159078f5de
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-16 00:36:34 +0800

    Added optimized solution for Scramble String

--- leetcode/[Hard] scramble-string_/Solution.java
+++ leetcode/[Hard] scramble-string_/Solution.java
@@ -1,25 +1,20 @@
 class Solution {
 	public boolean isScramble(String s1, String s2) {
-		if (s1.length() != s2.length()) return false;
-
-		return verify(s1, s2);
-	}
-
-	private boolean verify(String s1, String s2){
 		if (s1.equals(s2)) return true;
-		int[] count = new int[128];
 
-		for (int i = 0; i < s1.length(); ++i){
-			++count[s1.charAt(i)];
-			--count[s2.charAt(i)];
-		}
-		for (int i = 0; i < 128; ++i) if (count[i] != 0) return false;
+		int[] count = new int[26];
 
-		for (int i = 1; i < s1.length(); ++i){
-			if (verify(s1.substring(0, i) , s2.substring(0, i)) && verify(s1.substring(i), s2.substring(i))) return true;
-			if (verify(s1.substring(0, i), s2.substring(s1.length() - i)) && verify(s1.substring(i), s2.substring(0, s1.length() - i))) return true;
-		}
-		return false;
+                for (int i = 0; i < s1.length(); ++i){
+                        ++count[s1.charAt(i) - 'a'];
+                        --count[s2.charAt(i) - 'a'];
+                }
+                for (int i = 0; i < 26; ++i) if (count[i] != 0) return false;
+
+                for (int i = 1; i < s1.length(); ++i){
+                        if (isScramble(s1.substring(0, i) , s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i))) return true;
+                        if (isScramble(s1.substring(0, i), s2.substring(s1.length() - i)) && isScramble(s1.substring(i), s2.substring(0, s1.length() - i))) return true;
+                }
+                return false;
 	}
 }
 
