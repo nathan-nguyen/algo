36d778bc07e7fcc2e7154cb53d9e07b4b3fedc5e
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-17 16:37:16 +0800

    Added Permutations

--- codeforces/187A_/20180512_Solution.java
+++ codeforces/187A_/20180512_Solution.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args){
+		Solution solution = new Solution();
+		solution.solve();
+	}
+
+	private void solve(){
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 
 		int[] a = new int[n];
-		int[] pos = new int[n+1];
+		int[] pos = new int[n];
 
-		for (int i = 0; i < n; ++i) pos[in.nextInt()] = i;
+		for (int i = 0; i < n; ++i) {
+			int val = in.nextInt() - 1;
+			a[i] = val;
+			pos[val] = i;
+		}
 
-		for (int i = 0; i < n; ++i) a[pos[in.nextInt()]] = i;
+		for (int i = 0; i < n; ++i) a[pos[in.nextInt() - 1]] = i;
 
 		for (int i = 1; i < n; ++i){
 			if (a[i] < a[i-1]) {
