5657e5f8285cd647a50ce7caf0dfcf2ce368e2f9
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-23 11:30:32 +0800

    Added Best Time to Buy and Sell Stock IV

---
+++ leetcode/[Hard] best-time-to-buy-and-sell-stock-iv_/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+	public int maxProfit(int k, int[] p) {
+		if (k < 1 || p.length < 2) return 0;
+
+		int n = p.length;
+
+		if (k >= n / 2){
+			int total = 0;
+			for (int i = 1; i < n; ++i){
+				if (p[i] > p[i-1]) total += (p[i] - p[i-1]);
+			}
+			return total;
+		}
+
+		int hold;
+		int[][] notHold = new int[k+1][n];
+
+		for (int i = 1; i <= k; ++i){
+			hold = -p[0];
+			for (int j = 1; j < n; ++j){
+				notHold[i][j] = Math.max(hold + p[j], notHold[i][j-1]);
+				hold = Math.max(hold, notHold[i-1][j-1] - p[j]);
+			}
+		}
+		return notHold[k][n-1];
+	}
+}
