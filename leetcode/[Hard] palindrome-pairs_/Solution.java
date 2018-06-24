b559c3982553044ba4625dfaae28863082cfcbc1
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-06-24 22:33:28 +0800

    Added Palindrome Pairs

---
+++ leetcode/[Hard] palindrome-pairs_/Solution.java
@@ -0,0 +1,69 @@
+class Solution {
+	public List<List<Integer>> palindromePairs(String[] words) {
+		Map<String, Integer> map = new HashMap<>();
+		HashSet<String> set = new HashSet<>();
+ 
+		List<List<Integer>> l = new ArrayList<>();
+		for (int i = 0; i < words.length; ++i) map.put(words[i], i);
+
+		for (int i = 0; i < words.length; ++i) {
+			for (String s: getPairLeft(words[i])){
+				if (map.containsKey(s)){
+					int val = map.get(s);
+					if (set.contains(i + "_" + val)) continue;
+
+					List<Integer> pair = new ArrayList<>();
+					pair.add(i);
+					pair.add(val);
+					l.add(pair);
+					set.add(i + "_" + val);
+				}
+			}
+
+			for (String s: getPairRight(words[i])){
+				if (s.equals(words[i])) continue;
+                                if (map.containsKey(s)){
+					int val = map.get(s);
+					if (set.contains(val + "_" + i)) continue;
+
+                                        List<Integer> pair = new ArrayList<>();
+                                        pair.add(val);
+					pair.add(i);
+                                        l.add(pair);
+					set.add(val + "_" + i);
+                                }   
+                        }
+		}
+		return l;
+	}
+
+	private List<String> getPairLeft(String s){
+		List<String> result = new ArrayList<>();
+
+		if (!isPalindrome(s)) result.add((new StringBuilder(s)).reverse().toString());
+		else if (s.length() > 0) result.add("");
+
+		for (int i = s.length() - 1; i > 0; --i){
+			if (isPalindrome(s.substring(i))) result.add((new StringBuilder(s.substring(0, i))).reverse().toString()); 
+		}
+		return result;
+	}
+
+	private List<String> getPairRight(String s){
+		List<String> result = new ArrayList<>();
+
+		if (isPalindrome(s) && s.length() > 0) result.add("");
+
+		for (int i = 1; i < s.length(); ++i){
+			if (isPalindrome(s.substring(0, i))) result.add((new StringBuilder(s.substring(i))).reverse().toString());
+		}
+		return result;
+	}
+
+	private boolean isPalindrome(String s){
+		for (int i = 0; i < s.length() / 2; ++i){
+			if (s.charAt(i) != s.charAt(s.length() - 1 - i)) return false;
+		}
+		return true;
+	}
+}
