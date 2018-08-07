b7d0d8c25bd0e1021d987b2e2ea4cb9d06d133bd
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-08-08 02:10:42 +0800

    Added Largest Rectangle in Histogram

--- leetcode/[Hard] largest-rectangle-in-histogram_/Solution.java
+++ leetcode/[Hard] largest-rectangle-in-histogram_/Solution.java
@@ -1,28 +1,24 @@
 class Solution {
 	public int largestRectangleArea(int[] h) {
-		Stack<int[]> stack = new Stack<>();
+		Stack<Integer> stack = new Stack<>();
 
 		int max = 0;
 		for (int i = 0; i < h.length; ++i) {
-			while (stack.size() > 0 && stack.peek()[0] > h[i]){
-				int[] val = stack.pop();
-				int index = 0;
-				if (stack.size() == 0) index = -1;
-				else index = stack.peek()[1];
-				max = Math.max(max, val[0] * (i - index - 1));
+			if (stack.size() == 0) stack.push(i);
+			else {
+				while (stack.size() > 0 && h[stack.peek()] > h[i]) {
+					int k = stack.pop();
+					int leftBound = stack.size() > 0 ? stack.peek() : -1;
+					max = Math.max(max, h[k] * (i - leftBound - 1));
+				}
+				stack.push(i);
 			}
-			
-			if (stack.size() > 0 && stack.peek()[0] == h[i]) stack.pop();
-			stack.push(new int[] {h[i], i});
 		}
-
-		while (stack.size() > 0){
-                        int[] val = stack.pop();
-                        int index = 0;
-                        if (stack.size() == 0) index = -1; 
-        		else index = stack.peek()[1];
-                	max = Math.max(max, val[0] * (h.length - index - 1));
-        	}
-		return max;	
+		while (stack.size() > 0) {
+			int k = stack.pop();
+			int leftBound = stack.size() > 0 ? stack.peek() : -1;
+			max = Math.max(max, h[k] * (h.length - leftBound - 1));
+		}
+		return max;
 	}
 }
