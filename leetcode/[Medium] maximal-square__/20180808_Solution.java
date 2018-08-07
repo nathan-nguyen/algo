ba48217df3eb113550d53c6ac5d088e693b12370
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-08-08 01:42:15 +0800

    Added better solution for Maximal Square

--- leetcode/[Medium] maximal-square__/20180808_Solution.java
+++ leetcode/[Medium] maximal-square__/20180808_Solution.java
@@ -5,6 +5,17 @@ class Solution {
 		int n = matrix.length;
 		int m = matrix[0].length;
 
+		int[][] row = new int[n][m];
+		int[][] col = new int[n][m];
+
+		for(int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				if (matrix[i][j] == '0') continue;
+				row[i][j] = 1 + (j > 0 ? row[i][j-1] : 0);
+				col[i][j] = 1 + (i > 0 ? col[i-1][j] : 0);
+			}
+		}
+
 		int[][] dp = new int[n][m];
 		int max = 0;
 		for (int i = 0; i < n; ++i) {
@@ -13,7 +24,7 @@ class Solution {
 				dp[i][j] = 1;
 
 				if (i > 0 && j > 0) {
-					dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+					dp[i][j] = Math.min(Math.min(row[i][j] - 1, col[i][j] - 1), dp[i-1][j-1]) + 1;
 				}
 				max = Math.max(max, dp[i][j]);
 			}
