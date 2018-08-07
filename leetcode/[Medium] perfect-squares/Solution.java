3caa82617bd89ea2de616245dc90fbda9ff56522
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-08-08 00:49:42 +0800

    Added Perfect Squares

--- leetcode/[Medium] perfect-squares/Solution.java
+++ leetcode/[Medium] perfect-squares/Solution.java
@@ -1,14 +1,16 @@
 class Solution {
+	// Positive integer n;
 	public int numSquares(int n) {
-		int[] dp = new int[n+1];
-
-		for (int i = 1; i <= n; ++i){
-			int a = (int) Math.sqrt(i);
-			if (a * a == i) dp[i] = 1;
-			else {
-				dp[i] = Integer.MAX_VALUE;
-				for (int j = 1; j*j < i; ++j) dp[i] = Math.min(1 + dp[i - j * j], dp[i]);
+		int[] dp = new int[n + 1];
+		for (int i = 1; i <= n; ++i) {
+			int k = (int) Math.sqrt(i);
+			if (k * k == i) {
+				dp[i] = 1;
+				continue;
 			}
+			dp[i] = Integer.MAX_VALUE;
+
+			for (int j = 1; j * j < i; ++j) dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
 		}
 		return dp[n];
 	}
