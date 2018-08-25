ea7e7fa8af198f5a44b4f0b97c12891f58803fce
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-08-25 22:58:19 +0800

    Added Remove K Digits

--- leetcode/[Medium] remove-k-digits_/Solution.java
+++ leetcode/[Medium] remove-k-digits_/Solution.java
@@ -1,37 +1,37 @@
 class Solution {
-    public String removeKdigits(String s, int k) {
+	public String removeKdigits(String num, int k) {
+		int count = 0;
 
-	if (k >= s.length()) return "0";
-
-        int countZero = 0;
-        int zeroIndex = -1;
-	int del = 0;
-	for (int i = 0; del <= k && i < s.length(); ++i){
-		if (s.charAt(i) == '0') {
-			++countZero;
-			zeroIndex = i;
+		for (int i = 0; i < num.length(); ++i) {
+			if (num.charAt(i) == '0') {
+				k-= count;
+				count = 0;
+			}
+			else {
+				++count;
+				if (count > k) return removeKdigitsTail(num.substring(i - count + 1, num.length()), k);
+			}
 		}
-		else ++del;
+		return "0";
 	}
 
-	if (del <= k) return "0";
+	private String removeKdigitsTail(String num, int k) {
+		if (k == 0) return num;
 
-	s = s.substring(zeroIndex + 1, s.length());
-	k -= (zeroIndex + 1 - countZero);
+		StringBuilder sb = new StringBuilder();
 
-	if (k == 0) return s;
+		int cur = 0;
+		while (cur < num.length() && k > 0) {
+			if (sb.length() > 0 && sb.charAt(sb.length() - 1) > num.charAt(cur)) {
+				sb.setLength(sb.length() - 1);
+				--k;
+			}
+			else sb.append(num.charAt(cur++));
+		}
 
-	StringBuilder sb = new StringBuilder();
+		sb.append(num.substring(cur, num.length()));
+		sb.setLength(sb.length() - k);
 
-	for (int i = 0; i < s.length() - 1; ++i){
-		if (s.charAt(i) <= s.charAt(i + 1) || k <= 0) sb.append(s.charAt(i));
-		else --k;
+		return sb.length() == 0 ? "0" : sb.toString();
 	}
-
-	sb.append(s.charAt(s.length() - 1));
-	if (k > 0) sb.setLength (sb.length() - k);
-
-	return sb.toString();
-    }
 }
-
