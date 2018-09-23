3e2699ee3d90fd0376fc0dd05dffc5b08130b2f5
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-09-23 17:39:43 +0800

    Added optimized solution for Evaluating e^x

--- hackerrank/functional-programming/[Easy] eval-ex/Solution.scala
+++ hackerrank/functional-programming/[Easy] eval-ex/Solution.scala
@@ -1,19 +1,19 @@
 object Solution {
 
-  def f(x: Double, n: Int, result: Double, increase: Double): Double = n match {
-    case 11 => result
-    case _  => f(x, n + 1, result + increase, increase * x / n)
+  def f(x: Double): Double = {
+    def f(n: Int, result: Double, increase: Double): Double = n match {
+      case 11 => result
+      case _  => f(n + 1, result + increase, increase * x / n)
+    }
+    f(1, 0, 1)
   }
 
   def main(args: Array[String]) {
-    val stdin = scala.io.StdIn
+    val stdIn = scala.io.StdIn
+    val n = stdIn.readInt
 
-    val n = stdin.readLine.trim.toInt
-
-    for (nItr <- 1 to n) {
-      val x = stdin.readLine.trim.toDouble
-      println(f(x, 1, 0, 1))
-    }
+    (1 to n) foreach(_ => println(f(stdIn.readDouble)))
   }
 }
+
 
