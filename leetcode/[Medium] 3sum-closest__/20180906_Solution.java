5ca3ed07d1a022bcfea5e76e6ee1a59a4cee0c97
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-09-27 22:56:03 +0800

    Added 3Sum Closest

--- leetcode/[Medium] 3sum-closest__/20180906_Solution.java
+++ leetcode/[Medium] 3sum-closest__/20180906_Solution.java
@@ -1,20 +1,22 @@
 class Solution {
 	public int threeSumClosest(int[] nums, int target) {
 		Arrays.sort(nums);
+		int n = nums.length;
 
 		int min = nums[0] + nums[1] + nums[2];
 
-		for (int i = 0; i < nums.length - 2; ++i) {
+		for (int i = 0; i < n - 2; ++i){
 			int start = i + 1;
-			int end = nums.length - 1;
+			int end = n - 1;
 
-			while (start < end) {
+			while (start < end){
 				int sum = nums[i] + nums[start] + nums[end];
-				if (sum <= target) ++start;
-				else --end;
-				if (Math.abs(sum - target) < Math.abs(min - target)) min = sum;
+				if (sum >= target) --end;
+				if (sum < target) ++start;
+				if (Math.abs(min - target) > Math.abs(sum - target)) min = sum;	
 			}
 		}
 		return min;
 	}
+
 }
