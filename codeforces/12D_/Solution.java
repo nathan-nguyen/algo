d64de97c9411557249be4ee5fe09f45dc6ad12a7
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-10-27 12:17:08 +0800

    Added correct solution for Ball

--- codeforces/12D_/Solution.java
+++ codeforces/12D_/Solution.java
@@ -22,37 +22,34 @@ public class Solution {
         }
         for (int i = 0; i < n; ++i) a[i][2] = in.nextInt();
 
-        Arrays.sort(a, (u, v) -> Integer.compare(u[0], v[0]));
+        Arrays.sort(a, (u, v) -> Integer.compare(v[0], u[0]));
         Arrays.sort(c);
 
         int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
         map.put(c[0], count++);
-
         for (int i = 1; i < n; ++i) {
             if (c[i] != c[i-1]) map.put(c[i], count++);
         }
 
-        int[] tree = new int[n+1];
+        int[] tree = new int[count + 1];
         int answer = 0;
-
-        a[0][1] = map.get(a[0][1]);
-        for (int i = 1, j = 0; i < n; ++i) {
+        for (int i = 0, j = 0; i < n; ++i) {
             a[i][1] = map.get(a[i][1]);
-            while (j < n && a[j][0] < a[i][0]) {
-                update(tree, n, a[j][1], a[j][2]);
+            while (j < n && a[j][0] > a[i][0]) {
+                update(tree, count, a[j][1], a[j][2]);
                 ++j;
             }
 
-            if (get(tree, n, a[i][1] - 1) > a[i][2]) ++answer;
+            if (get(tree, a[i][1] - 1) > a[i][2]) ++answer;
         }
 
         System.out.println(answer);
     }
 
-    private int get(int[] tree, int n, int index) {
+    private int get(int[] tree, int index) {
         ++index;
-        int result = Integer.MIN_VALUE;
+        int result = 0;
         while (index > 0) {
             result = Math.max(result, tree[index]);
             index -= index & (-index);
@@ -67,4 +64,5 @@ public class Solution {
             index += index & (-index);
         }
     }
+
 }
