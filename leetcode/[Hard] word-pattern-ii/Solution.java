ae947911a47eb805d9e7aa3f1b062452b6595ac4
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-11-08 22:43:23 +0800

    Added Word Pattern II

---
+++ leetcode/[Hard] word-pattern-ii/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean wordPatternMatch(String pattern, String str) {
+        return wordPatternMatch(pattern, str, new HashMap<>(), new HashSet<>());
+    }
+
+    public boolean wordPatternMatch(String pattern, String str, Map<Character, String> map, Set<String> set){
+        if (pattern.length() == 0) return str.length() == 0;
+        
+        char c = pattern.charAt(0);
+        if (map.containsKey(c)) {
+            String value = map.get(c);
+            if (value.length() > str.length()) return false;
+            return value.equals(str.substring(0, value.length())) && 
+                wordPatternMatch(pattern.substring(1), str.substring(value.length()), map, set);
+        }
+        else {
+            for (int i = 1; i <= str.length(); ++i) {
+                String value = str.substring(0, i);
+                if (set.contains(value)) continue;
+                set.add(value);
+                map.put(c, value);
+                if (wordPatternMatch(pattern.substring(1), str.substring(i), map, set)) return true;
+                map.remove(c);
+                set.remove(value);
+            }
+        }
+        return false;
+    }
+}
