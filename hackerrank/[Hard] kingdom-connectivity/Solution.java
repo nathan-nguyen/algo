8d056c8dd66068fd07faefdaae277058271d6f4c
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-11-18 01:36:15 +0800

    Refactored source code for Kingdom Connectivity

--- hackerrank/[Hard] kingdom-connectivity/Solution.java
+++ hackerrank/[Hard] kingdom-connectivity/Solution.java
@@ -25,19 +25,28 @@ public class Solution {
             reverseEdge.get(b).add(a);
         }
 
+        // Check whether there is path from node i to n-1
         boolean[] reachable = new boolean[n];
         updateReachable(n-1, reachable, reverseEdge);
 
-        // Find loop
+
+        // Remove unreachable nodes
+        for (int i = 0; i < n; ++i) {
+            List<Integer> list = new ArrayList<>();
+            for (int j: edge.get(i)) if (reachable[j]) list.add(j);
+            edge.set(i, list);
+        }
+
+        // Find loop from node 0
         boolean[] visited = new boolean[n];
-        if (containLoop(new HashSet<>(), 0, visited, edge, reachable)) {
+        if (containLoop(new HashSet<>(), 0, visited, edge)) {
             System.out.println("INFINITE PATHS");
             return;
         }
 
         int[] value = new int[n];
         Arrays.fill(value, -1);
-        System.out.println(dfs(0, value, edge, n-1, reachable));
+        System.out.println(dfs(0, value, edge, n-1));
     }
 
     private void updateReachable(int cur, boolean[] reachable, List<List<Integer>> edge) {
@@ -46,25 +55,21 @@ public class Solution {
         for (int next: edge.get(cur)) updateReachable(next, reachable, edge);
     }
 
-    private int dfs(int cur, int[] value, List<List<Integer>> edge, int target, boolean[] reachable) {
+    private int dfs(int cur, int[] value, List<List<Integer>> edge, int target) {
         if (value[cur] >= 0) return value[cur];
-        if (!reachable[cur]) return 0;
         value[cur] = 0;
-        if (cur == target) {
-            value[cur] = 1;
-            return value[cur];
-        }
-        for (int next: edge.get(cur)) value[cur] = (value[cur] + dfs(next, value, edge, target, reachable)) % 1000_000_000;
+        if (cur == target) value[cur] = 1;
+        else for (int next: edge.get(cur)) value[cur] = (value[cur] + dfs(next, value, edge, target)) % 1000_000_000;
         return value[cur];
     }
 
-    private boolean containLoop(Set<Integer> set, int cur, boolean[] visited, List<List<Integer>> edge, boolean[] reachable) {
+    private boolean containLoop(Set<Integer> set, int cur, boolean[] visited, List<List<Integer>> edge) {
         if (visited[cur] && set.contains(cur)) return true;
-        if (visited[cur] || !reachable[cur]) return false;
+        if (visited[cur]) return false;
         visited[cur] = true;
         set.add(cur);
         for (int next: edge.get(cur)) {
-            if (containLoop(set, next, visited, edge, reachable)) return true;
+            if (containLoop(set, next, visited, edge)) return true;
         }
         set.remove(cur);
         return false;
