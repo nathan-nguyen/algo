c22c2c43230c17c6baf745a31c417fdc519bf245
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-11-20 23:16:53 +0800

    Added Pictures with Kittens

--- codeforces/1077F2_/Solution.java
+++ codeforces/1077F2_/Solution.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
@@ -15,21 +14,21 @@ public class Solution {
         int[] a = new int[n];
         for (int i = 0; i < n; ++i) a[i] = in.nextInt();
 
-        int[][] dp = new int[n][x+1];
-        for (int i = 0; i < n; ++i) {
-            Arrays.fill(dp[i], Integer.MIN_VALUE);
-            dp[i][1] = a[i];
-        }
+        long[][] dp = new long[n][x+1];
+        for (int i = 0; i < n; ++i) Arrays.fill(dp[i], Long.MIN_VALUE);
+        for (int i = 0; i < k; ++i) dp[i][1] = a[i];
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 2; j <= x; ++j) {
-                for (int d = 1; d <=k; ++d) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i-d][j-1] + a[i]);
-                }
+        for (int j = 2; j <= x; ++j) {
+            Deque<Integer> deque = new LinkedList<>();
+            for (int i = 0; i < n; ++i) {
+                if (!deque.isEmpty() && dp[deque.peekFirst()][j-1] != Long.MIN_VALUE) dp[i][j] = Math.max(dp[i][j], dp[deque.peekFirst()][j-1] + a[i]);
+                while (!deque.isEmpty() && deque.peekFirst() <= i - k) deque.pollFirst();
+                while (!deque.isEmpty() && dp[deque.peekLast()][j - 1] <= dp[i][j - 1]) deque.pollLast();
+                deque.offerLast(i);
             }
         }
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < k; ++i) max = Math.max(max, dp[n - 1 - i][x])
-        System.out.println(max == Integer.MIN_VALUE ? 1 : max);
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < k; ++i) max = Math.max(max, dp[n - 1 - i][x]);
+        System.out.println(max == Long.MIN_VALUE ? -1 : max);
     }
 }
