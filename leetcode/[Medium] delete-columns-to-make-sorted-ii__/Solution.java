23a32a5b8ce2783d75469b5174c2ea51697c8563
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-12-30 11:36:03 +0800

    Added Delete Columns to Make Sorted II

--- leetcode/[Medium] delete-columns-to-make-sorted-ii__/Solution.java
+++ leetcode/[Medium] delete-columns-to-make-sorted-ii__/Solution.java
@@ -3,28 +3,19 @@ class Solution {
         int n = a.length;
         int m = a[0].length();
 
-        List<int[]> list = new ArrayList<>();
-        list.add(new int[]{0, n - 1});
-
+        boolean[] next = new boolean[n-1];
         int count = 0;
         outer:for (int i = 0; i < m; ++i) {
-            for (int[] e: list) {
-                for (int j = e[0]; j < e[1]; ++j) if (a[j].charAt(i) > a[j+1].charAt(i)) {
+            for (int j = 0; j < n - 1; ++j) {
+                if (next[j]) continue;
+                if (a[j].charAt(i) > a[j+1].charAt(i)) {
                     ++count;
                     continue outer;
                 }
             }
-
-            List<int[]> next = new ArrayList<>();
-            for (int[] e: list) {
-                for (int j = e[0] + 1; j <= e[1]; ++j) if (a[j].charAt(i) == a[j-1].charAt(i)) {
-                    int start = j-1;
-                    while (j <= e[1] && a[j].charAt(i) == a[j-1].charAt(i)) ++j;
-                    next.add(new int[]{start, j - 1});
-                }
+            for (int j = 0; j < n - 1; ++j) {
+                if (!next[j] && a[j].charAt(i) != a[j+1].charAt(i)) next[j] = true;
             }
-            if (next.size() == 0) return count;
-            list = next; 
         }
         return count;
     }
