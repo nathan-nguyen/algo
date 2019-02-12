88747a87e30d590fe74733fb6c0d88a9ee7c4b7f
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-02-12 21:48:34 +0800

    Added Number of Ways

--- codeforces/466C/Solution.java
+++ codeforces/466C/Solution.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int[] a = new int[n];
-		
-		long total = 0;
-		for (int i = 0; i < n; ++i){
-			a[i] = in.nextInt();
-			total += a[i];
-		}
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solve();
+    }
 
-		if (n < 3 || total % 3 != 0){
-			System.out.println(0);
-			return;
-		}
+    private void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
 
-		long result = 0;
-		long localTotal = 0;
-		int count = 0;
-		for (int i = 0; i < n; ++i){
-			localTotal += a[i];
-			if (localTotal == total / 3 * 2 && i > 0 && i < n-1) result += count;
-			if (localTotal == total / 3) ++count;
-		}
-		System.out.println(result);
-	}
+        long total = 0;
+        for (int i = 0; i < n; ++i) {
+            a[i] = in.nextInt();
+            total += a[i];
+        }
+
+        if (total % 3 != 0) {
+            System.out.println(0);
+            return;
+        }
+
+        long result = 0, sum = 0;
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += a[i];
+            if (sum == total * 2 / 3 && i > 0 && i < n - 1) result += count;
+            if (sum == total / 3) ++count;
+        }
+
+        System.out.println(result);
+    }
 }
