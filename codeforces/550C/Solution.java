a6c6f8f2081899f8a77cd911fa4764cbb1d7dc08
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-02-18 09:00:45 +0800

    Added Divisibility by Eight

--- codeforces/550C/Solution.java
+++ codeforces/550C/Solution.java
@@ -1,58 +1,34 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		String n = in.next();
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int n = s.length();
+        String ans = solution.solve(n, s);
+        System.out.println(ans.equals("-1") ? "NO" : "YES\n" + ans);
+    }
 
-		int result = solve(n);
-		if (result >=0) System.out.println("YES\n" + result);
-		else System.out.println("NO");
-	}
+    private String solve(int n, String s) {
+        boolean[] one = new boolean[10];
+        boolean[] two = new boolean[100];
 
-	public static int solve(String n){
-		int l = n.length();
-		int[] first = new int[8];
-		int[] second = new int[8];
+        for (int i = n - 1; i >= 0; --i) {
+            int d = s.charAt(i) - '0';
+            if (d % 8 == 0) return String.valueOf(d);
 
-		for (int i = 0; i < 8; ++i) {
-			first[i] = -1;
-			second[i] = -1;
-		}
+            for (int j = 0; j < 100; ++j) {
+                if (two[j] && (d * 100 + j) % 8 == 0) return s.substring(0, i + 1) + j;
+            }
 
-		int firstChar = n.charAt(l-1) - '0';
-		if (firstChar % 8 == 0) return firstChar;
-
-		first[firstChar % 8] = firstChar;
-
-		if (l >= 2){
-			int secondChar = n.charAt(l-2) - '0';
-			if (secondChar % 8 == 0) return secondChar;
-			int lastTwo = secondChar * 10 + firstChar;
-			if (lastTwo % 8 == 0) return lastTwo;
-			first[secondChar % 8] = secondChar;
-			second[lastTwo % 8] = lastTwo;
-		}
-
-		for (int i = l-3; i >= 0; --i){
-			int nextDigit = n.charAt(i) - '0';
-			if (nextDigit % 8 == 0) return nextDigit;
-			for (int j = 0; j < 8 ; ++j){
-				if (second[j] > 0) {
-					int lastThree = nextDigit * 100 + second[j];
-					if (lastThree % 8 == 0) return lastThree;
-				}
-			}
-			for (int j = 0; j < 8; ++j){
-				if (first[j] > 0){
-					int lastTwo = nextDigit * 10 + first[j];
-					if (lastTwo % 8 == 0) return lastTwo;
-					second[lastTwo % 8] = lastTwo;
-				}
-			}
-			first[nextDigit % 8] = nextDigit;
-		}
-
-		return -1;
-	}
+            for (int j = 0; j < 10; ++j) {
+                if (!one[j]) continue;
+                if ((d * 10 + j) % 8 == 0) return String.valueOf(d * 10 + j);
+                two[d * 10 + j] = true;
+            }
+            one[d] = true;
+        }
+        return "-1";
+    }
 }
