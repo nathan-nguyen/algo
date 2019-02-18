e51be6db43b51bb7931f01cf5892a86ecdaa6639
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-02-18 20:30:32 +0800

    Added A and B and Team Training

--- codeforces/519C/Solution.java
+++ codeforces/519C/Solution.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int m = in.nextInt();
-		if (m >= 2 * n) System.out.println(n);
-		else if (n >= 2 * m) System.out.println(m);
-		else System.out.println((n+m) / 3);
-	}
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solve();
+    }
+
+    private void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int min = Math.min(n, m);
+        int max = Math.max(n, m);
+        if (min * 2 < max) max = min * 2;
+        System.out.println((min + max) / 3);
+    }
 }
+
