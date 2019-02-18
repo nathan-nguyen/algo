46731e65e67f10254e3c3782122a20386959b058
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-02-18 21:40:56 +0800

    Added Building Permutation

--- codeforces/285C/Solution.java
+++ codeforces/285C/Solution.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
-import java.util.PriorityQueue;
 
 public class Solution {
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		for (int i = 0; i < n; ++i) pq.add(in.nextInt());
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solve();
+    }
 
-		long result = 0;
-		for (int i = 0; i < n; ++i) result += Math.abs(pq.poll() - i - 1);
-		System.out.println(result);
-	}
+    private void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long sum = 0;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; ++i) list.add(in.nextInt());
+        Collections.sort(list);
+
+        for (int i = 0; i < n; ++i) sum += Math.abs(list.get(i) - i - 1);
+        System.out.println(sum);
+    }
 }
