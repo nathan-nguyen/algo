a6ef5e73a9da9ff286433a3ae6f5d9d91204d59a
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-02-18 21:08:26 +0800

    Added k-Tree

--- codeforces/431C/Solution.java
+++ codeforces/431C/Solution.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static int MOD = 1000000007;
+    private static final int MOD = 1000000007;
 
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int k = in.nextInt();
-		int d = in.nextInt();
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solve();
+    }
 
-		int[] cd = new int[n+1];
-		int[] ncd = new int[n+1];
+    private void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int d = in.nextInt();
 
-		cd[0] = 1;
-		ncd[0] = 1;
+        int[] high = new int[n + 1];
+        int[] low = new int[n+1];
+        low[0] = high[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int x = 1; x <= k; ++x) {
+                if (i >= x) high[i] = (high[i] + high[i-x]) % MOD;
+                if (i >= x && x < d) low[i] = (low[i] + low[i-x]) % MOD;
+            }
+        }
 
-		for (int i = 1; i <= n; ++i){
-			for (int j = 1; j <= k; ++j){
-				if (i - j < 0) break;
-				cd[i] = (cd[i] + cd[i-j]) % MOD;
-				if (j < d) ncd[i] = (ncd[i] + ncd[i-j]) % MOD;
-			}
-		}
-		System.out.println(( cd[n] + MOD - ncd[n]) % MOD);
-	}
+        System.out.println((high[n] - low[n] + MOD) % MOD);
+    }
 }
