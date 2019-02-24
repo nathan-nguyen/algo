5030a398e073dd8e8a8388ffc5fcd3564c6e189d
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-02-24 14:49:45 +0800

    Added Distinct Subsequences

--- leetcode/[Hard] distinct-subsequences/Solution.java
+++ leetcode/[Hard] distinct-subsequences/Solution.java
@@ -1,14 +1,17 @@
 class Solution {
-	public int numDistinct(String s, String t) {
-		int[][] dp = new int[t.length() + 1][s.length() + 1];
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
 
-		for (int i = 0; i <= s.length(); ++i) dp[0][i] = 1;
-		for (int i = 1; i <= t.length(); ++i){
-			for (int j = 1; j <= s.length(); ++j){
-				dp[i][j] = dp[i][j-1];
-				if (t.charAt(i - 1) == s.charAt(j - 1)) dp[i][j] += dp[i-1][j-1];
-			}
-		}
-		return dp[t.length()][s.length()];
-	}
+        for (int i = 0; i <= n; ++i) dp[i][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) dp[i][j] += dp[i-1][j-1];
+                dp[i][j] += dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
 }
+
