566a259f699f78f895abb1045bca4bc3319a9774
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-02-28 08:40:31 +0800

    Added Minimum Cost to Hire K Workers

--- leetcode/[Hard] minimum-cost-to-hire-k-workers/Solution.java
+++ leetcode/[Hard] minimum-cost-to-hire-k-workers/Solution.java
@@ -1,34 +1,26 @@
 class Solution {
-	public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-		int n = quality.length;
-
-		int[][] rate = new int[n][2];
-		for (int i = 0; i < n; ++i){
-			rate[i][0] = quality[i];
-			rate[i][1] = wage[i];
-		}
-
-		Arrays.sort(rate, (u, v) -> (u[1] * v[0] == u[0] * v[1]) ? Integer.compare(u[0], v[0]) : Integer.compare(u[1] * v[0], u[0] * v[1]));
-
-		int sum = 0;
-
-		Queue<Integer> pq = new PriorityQueue<>((u, v) -> Integer.compare(v, u));
-
-		for (int i = 0; i < k; ++i){
-			sum += rate[i][0];
-			pq.offer(rate[i][0]);
-		}
-
-		double minRate = (double) rate[k-1][1] / rate[k-1][0];
-		double minCost = minRate * sum;
-
-		for (int i = k; i < n; ++i){
-			minRate = (double) rate[i][1] / rate[i][0];
-			sum += rate[i][0];
-			pq.offer(rate[i][0]);
-			sum -= pq.poll();
-			minCost = Math.min(minCost, minRate * sum);
-		}
-		return minCost;
+    public double mincostToHireWorkers(int[] q, int[] w, int k) {
+        int n = q.length;
+        int[][] r = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            r[i][0] = w[i];
+            r[i][1] = q[i];
         }
+        Arrays.sort(r, (u, v) -> Integer.compare(u[0] * v[1], u[1] * v[0]));
+        Queue<Integer> pq = new PriorityQueue<>((u, v) -> Integer.compare(v, u));
+
+        int sum = 0;
+        double result = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; ++i) {
+            pq.offer(r[i][1]);
+            sum += r[i][1];
+            if (i >= k - 1) {
+                result = Math.min(result, 1.0 * sum * r[i][0] / r[i][1]);
+                sum -= pq.poll();
+            }
+        }
+        return result;
+    }
 }
+
