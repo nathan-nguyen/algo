316f5d1363f3489527b8ebf8dd7c3afbcac921e6
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-05-24 09:28:56 +0800

    Added Best Time to Buy and Sell Stock with Transaction Fee

--- leetcode/[Medium] best-time-to-buy-and-sell-stock-with-transaction-fee_/Solution.java
+++ leetcode/[Medium] best-time-to-buy-and-sell-stock-with-transaction-fee_/Solution.java
@@ -1,14 +1,12 @@
 class Solution {
-	public int maxProfit(int[] p, int fee) {
-		if (p.length == 0) return 0;
-
-		int notBuy = 0;
-		int hold = -p[0];
-
-		for (int i = 1; i < p.length; ++i){
-			notBuy = Math.max(notBuy, hold + p[i] - fee);
-			hold = Math.max(hold, notBuy - p[i]);
-		}
-		return notBuy;
-	}
+    public int maxProfit(int[] p, int f) {
+        int hold = -p[0];
+        int notHold = 0;
+        for (int i: p) {
+            hold = Math.max(hold, notHold - i);
+            notHold = Math.max(notHold, hold + i - f);
+        }
+        return notHold;
+    }
 }
+
