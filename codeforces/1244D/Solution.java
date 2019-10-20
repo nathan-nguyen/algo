1c096eb4873de4f1764e7d319c2e8da08efa3f76
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2019-10-21 01:57:32 +0800

    Added working version of Paint the Tree

--- codeforces/1244D/Solution.java
+++ codeforces/1244D/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +9,7 @@ public class Solution {
         System.out.println(solution.solve());
     }
 
-    private int solve() {
+    private String solve() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[][] cost = new int[3][n];
@@ -21,6 +22,9 @@ public class Solution {
         }
 
         int[] degree = new int[n];
+        int[] color = new int[n];
+        int[] min = new int[n];
+        Arrays.fill(color, -1);
 
         for (int i = 0; i < n - 1; ++i) {
             int a = in.nextInt() - 1;
@@ -31,17 +35,68 @@ public class Solution {
             ++degree[b];
         }
 
-        for (int i = 0; i < n; ++i) if (degree[i] > 2) return -1;
+        for (int i = 0; i < n; ++i) if (degree[i] > 2) return "-1";
 
+        List<Integer> order = new ArrayList<>();
+
+        boolean[] visited = new boolean[n];
+        findOrder(g, 0, visited, order);
+
+        dfs(g, 0, color, min, cost, order);
+
+        StringBuilder result = new StringBuilder();
+        long total = 0;
+        for (int i = 0; i < n; ++i) total += cost[min[i]][i];
+
+        result.append(total + "\n");
+        for (int i = 0; i < n; ++i) result.append((min[i] + 1) + " ");
+
+        return result.toString();
+    }
+
+    private void compare(int[] min, int[] color, int[][] cost) {
+        long totalMin = 0, total = 0;
+        for (int i = 0; i < min.length; ++i) {
+            totalMin += cost[min[i]][i];
+            total += cost[color[i]][i];
+        }
+        if (total < totalMin) {
+            for (int i = 0; i < min.length; ++i) min[i] = color[i];
+        }
+    }
+
+    private void dfs(List<List<Integer>> g, int current, int[] color, int[] min, int[][] cost, List<Integer> order) {
+        if (current == g.size()) {
+            compare(min, color, cost);
+            return;
+        }
+
+        int node = order.get(current);
         for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < n; ++j) {
-                // Start with vertex with degree = 1
-                if (degree[j] == 1) {
-                    break;
-                }
+            if (isValidColor(i, g, node, color)) {
+                color[node] = i;
+                dfs(g, current + 1, color, min, cost, order);
+            }
+            color[node] = -1;
+        }
+    }
+
+    private void findOrder(List<List<Integer>> g, int node, boolean[] visited, List<Integer> order) {
+        if (visited[node]) return;
+        visited[node] = true;
+        order.add(node);
+        for (int k: g.get(node)) findOrder(g, k, visited, order);
+    }
+
+    private boolean isValidColor(int c, List<List<Integer>> g, int node, int[] color) {
+        if (color[node] == c) return false;
+        for (int k: g.get(node)) {
+            if (color[k] == c) return false;
+            for (int x: g.get(k)) {
+                if (color[x] == c) return false;
             }
         }
-        return 0;
+        return true;
     }
 
 }
