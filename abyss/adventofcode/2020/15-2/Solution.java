3ce8bf70a74de941ac095a31df811ec435b4a75f
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2020-12-15 23:37:50 +0800

    Added ATC 2020 day 15

---
+++ abyss/adventofcode/2020/15-2/Solution.java
@@ -0,0 +1,34 @@
+import  java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solve();
+    }
+
+    private void solve() {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String[] sp = s.split(",");
+        Map<Integer, Queue<Integer>> map = new HashMap<>();
+
+        int cur = 0;
+        for (int i = 1; i <= 30_000_000; ++i) {
+            if (i <= sp.length) cur = Integer.parseInt(sp[i - 1]);
+            else if (map.get(cur).size() == 1) cur = 0;
+            else {
+                Queue<Integer> queue = map.get(cur);
+                int t1 = queue.poll();
+                int t2 = queue.peek();
+                cur = t2 - t1;
+            }
+            add(map, cur, i);
+        }
+        System.out.println(cur);
+    }
+
+    private void add(Map<Integer, Queue<Integer>> map, int cur, int pos) {
+        if (!map.containsKey(cur)) map.put(cur, new LinkedList<>());
+        map.get(cur).offer(pos);
+    }
+}
